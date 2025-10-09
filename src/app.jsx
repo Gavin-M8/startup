4d90cacc0ce0e5c691dfce62aa088e145800dbd6
Gavin Mobley <gavinwm@byu.edu>
2025-10-08 22:35:32 -0600

    fixed router paths

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
-import { BrowserRouter } from 'react-router-dom';
+
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Cook } from './cook/cook';
+import { Home } from './home/home';
 
 export default function App() {
   return (
     <BrowserRouter>
-        <body className="bg-light text-dark">
-
+        <div className=" body bg-light text-dark">
             <header className="bg-dark text-light container-fluid">
             <nav className="navbar navbar-dark">
                 <div className="nav-div">
                     <menu className="navbar-nav">
                         <NavLink id="brand" className="navbar-brand" to="#">Recipes Please</NavLink>
-                        <li className="nav-item"><NavLink className="nav-link active" to="index.html" >Home</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link active" to="login.html" >Login</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link active" to="cook.html" >Cook</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link active" to="/" >Home</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link active" to="/login" >Login</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link active" to="/cook" >Cook</NavLink></li>
                     </menu>
                 </div>
             </nav>
             <hr />
             </header>
 
-            import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-            import { Login } from './login/login';
-            import { Cook } from './cook/cook';
-            import { Home } from './home/home';
-
+            <Routes>
+            <Route path='/' element={<Home />} exact />
+            <Route path='/login' element={<Login />} />
+            <Route path='/cook' element={<Cook />} />
+            <Route path='*' element={<NotFound />} />
+            </Routes>
 
             <footer className="bg-dark text-light">
                 <hr />
@@ -35,8 +39,11 @@ export default function App() {
                 <NavLink className="github nav-link" to="https://github.com/Gavin-M8/startup?tab=readme-ov-file"> -- GitHub -- </NavLink> 
                 <hr />
             </footer>
-
-        </body>
+        </div>
     </BrowserRouter>
     );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
