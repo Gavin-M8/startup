8959ba6b2db51a625268ac0bacd2ed8ad5572380
Gavin Mobley <gavinwm@byu.edu>
2025-10-08 22:57:15 -0600

    readied files for deployment

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import "./login.css";
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Login displayed here</div>
+    <main className="bg-light text-dark">
+        <h2>Please Log In</h2>
+            <form method="get" action="cook.html">
+                <div>
+                    <input type="text" className="form-control" placeholder="email" />
+                </div>
+                <div>
+                    <input type="password" className="form-control" placeholder="password" />
+                </div>
+                <button className="btn btn-dark shadow" type="submit">Login</button>
+                <button className="btn btn-dark shadow" type="submit">Create</button>
+            </form>
+        <br />
     </main>
   );
 }
