1a6bf0731b5b03b2ecbb5b9a11ab488c760a52b5
Gavin Mobley <gavinwm@byu.edu>
2025-10-08 22:22:18 -0600

    added browser router

--- src/app.jsx
+++ src/app.jsx
@@ -1,37 +1,42 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
   return (
-  <body className="bg-light text-dark">
+    <BrowserRouter>
+        <body className="bg-light text-dark">
 
-        <header className="bg-dark text-light container-fluid">
-        <nav className="navbar navbar-dark">
-            <div className="nav-div">
-                <menu className="navbar-nav">
-                    <a id="brand" className="navbar-brand" href="#">Recipes Please</a>
-                    <li className="nav-item"><a className="nav-link active" href="index.html" >Home</a></li>
-                    <li className="nav-item"><a className="nav-link active" href="login.html" >Login</a></li>
-                    <li className="nav-item"><a className="nav-link active" href="cook.html" >Cook</a></li>
-                </menu>
-            </div>
-        </nav>
-        <hr />
-        </header>
-
-
-        <main className="bg-light text-dark"> Content Here </main>
-
-
-        <footer className="bg-dark text-light">
+            <header className="bg-dark text-light container-fluid">
+            <nav className="navbar navbar-dark">
+                <div className="nav-div">
+                    <menu className="navbar-nav">
+                        <NavLink id="brand" className="navbar-brand" to="#">Recipes Please</NavLink>
+                        <li className="nav-item"><NavLink className="nav-link active" to="index.html" >Home</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link active" to="login.html" >Login</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link active" to="cook.html" >Cook</NavLink></li>
+                    </menu>
+                </div>
+            </nav>
             <hr />
-            <span>Gavin Mobley</span>
-            <br />
-            <a className="github nav-link" href="https://github.com/Gavin-M8/startup?tab=readme-ov-file"> -- GitHub -- </a> 
-            <hr />
-        </footer>
+            </header>
 
-    </body>
+            import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+            import { Login } from './login/login';
+            import { Cook } from './cook/cook';
+            import { Home } from './home/home';
+
+
+            <footer className="bg-dark text-light">
+                <hr />
+                <span>Gavin Mobley</span>
+                <br />
+                <NavLink className="github nav-link" to="https://github.com/Gavin-M8/startup?tab=readme-ov-file"> -- GitHub -- </NavLink> 
+                <hr />
+            </footer>
+
+        </body>
+    </BrowserRouter>
     );
 }
---
+++ src/cook/cook.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Cook() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Cook displayed here</div>
+    </main>
+  );
+}
