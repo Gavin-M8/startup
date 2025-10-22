ba207c5aa01f866591df113ea99d1ba4747cc02e
Gavin Mobley <gavinwm@byu.edu>
2025-10-21 19:51:04 -0600

    added authenticated authState fucntionality

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 
+import './authenticated.css';
 
 export function Authenticated(props) {
-  return (<div></div>);
+  const navigate = useNavigate();
+
+  function logout() {
+    localStorage.removeItem('userName');
+    props.onLogout();
+  }
+
+  return (
+    <div>
+      <div className='playerName'>{props.userName}</div>
+      <Button variant='primary' onClick={() => navigate('/play')}>
+        Play
+      </Button>
+      <Button variant='secondary' onClick={() => logout()}>
+        Logout
+      </Button>
+    </div>
+  );
 }
