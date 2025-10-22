90d82c2c7a0d67e7e19d4ef1b8121e6981ba93de
Gavin Mobley <gavinwm@byu.edu>
2025-10-22 15:25:48 -0600

    removed commented out code

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -25,16 +25,3 @@ export function Login({ userName, authState, onAuthChange }) {
     </main>
   );
 }
-
-/* <h2>Please Log In</h2>
-            <form method="get" action="cook.html">
-                <div>
-                    <input type="text" className="form-control" placeholder="email" />
-                </div>
-                <div>
-                    <input type="password" className="form-control" placeholder="password" />
-                </div>
-                <button className="btn btn-dark shadow" type="submit">Login</button>
-                <button className="btn btn-dark shadow" type="submit">Create</button>
-            </form>
-        <br /> */
