94daaeb2916422f9ea8b62ccf9412adcb319c84c
Gavin Mobley <gavinwm@byu.edu>
2025-10-22 23:00:16 -0600

    added mock quote functionality

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 
 export function Home() {
+
+    const quotes = [
+    { text: "Cooking is love made visible.", author: "Unknown" },
+    { text: "People who love to eat are always the best people.", author: "Julia Child" },
+    { text: "A recipe has no soul. You, as the cook, must bring soul to the recipe.", author: "Thomas Keller" },
+    { text: "The secret ingredient is always love.", author: "Grandma" },
+    { text: "To eat is a necessity, but to cook is an art.", author: "Unknown" },
+  ];
+
+  const [quote, setQuote] = useState({ text: "", author: "" });
+
+   useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setQuote(quotes[randomIndex]);
+  }, []);
+    
   return (
     <main className="bg-light text-dark">
         <div className="main-container">
@@ -15,7 +32,10 @@ export function Home() {
                 <img className="shadow" src="/lechef.png" alt="chef" border="1"/>
             </div>
 
-            <h4 align="center">some quote</h4><br /><h5 align="center">- some person</h5>
+            <h4 align="center">"{quote.text}"</h4>
+            <br />
+            <h5 align="center">- {quote.author}</h5>
+
         </div>
     </main>
   );
