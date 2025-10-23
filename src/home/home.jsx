8e57c7dd616c82db806e0f3bb771264251133792
Gavin Mobley <gavinwm@byu.edu>
2025-10-22 22:10:41 -0600

    updated home page styling

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -7,11 +7,15 @@ export function Home() {
             <div className="welcome">
                 <h1 align="center"><strong>Welcome to Recipes Please!</strong></h1>
                     <hr />
-                <h3 align="center"><em>"To chef one's food is one's own destiny."</em><br /> - Anonymous</h3>
+                <h3 align="center"><em>"To chef one's food is one's own destiny."</em></h3>
+                <br />
+                
             </div>
             <div className="pizza">
-                <img className="shadow" src="recipes-please-icon.png" alt="chef" border="1"/>
+                <img className="shadow" src="/lechef.png" alt="chef" border="1"/>
             </div>
+
+            <h4 align="center">some quote</h4><br /><h5 align="center">- some person</h5>
         </div>
     </main>
   );
