d19b18e14cd2f457d16eceb3e87e4c4ca23d6d54
Gavin Mobley <gavinwm@byu.edu>
2025-11-05 10:08:06 -0700

    set up express

--- service/index.js
+++ service/index.js
@@ -1,2 +1,25 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+// list of users
+let users = [];
+
+// set backend port to 4000
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// set up express
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// endpoints
